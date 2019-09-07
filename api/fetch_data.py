b025b157c499e55c393f2339d401469c5fab6c58
Druval Carvalho <durvalcsouza@outlook.com>
2019-09-07 01:12:03 -0300

    TMP COMMIT [REMOVE LATER]

--- api/fetch_data.py
+++ api/fetch_data.py
@@ -6,16 +6,54 @@ from exchange_locales import (
     CurrencyConverter
 )
 
+import pickle
+import matplotlib.pyplot as plt
+
+import networkx as nx
+from networkx.algorithms.shortest_paths.dense import floyd_warshall_predecessor_and_distance
+
 if __name__ == '__main__':
-    
-    FixerIO()
-    print('25%')
+    data = FixerIO().edges + CurrencyLayer().edges + ExchangeRatesAPI().edges + CurrencyConverter().edges
 
-    CurrencyLayer()
-    print('50%')
-    
-    ExchangeRatesAPI()
-    print('75%')
+    # Arestas que unem as moedas independente da casa de câmbio
+    # BRL -> BRL_FO, BRL_FO -> BRL
+    unique = set()
+    for currencyA, currencyB, rate in data:
+        moneyA = currencyA[:3]
+        moneyB = currencyB[:3]
 
-    CurrencyConverter()
-    print('100%')
+        unique.add((moneyA, currencyA, 1))
+        unique.add((currencyA, moneyA, 1))
+
+        unique.add((moneyB, currencyB, 1))
+        unique.add((currencyB, moneyB, 1))
+    
+    data += list(unique)
+    
+    # dicionário para achar a taxa de câmbio de acordo com a transação
+    convert = {}
+    for currencyA, currencyB, rate in data:
+        convert[(currencyA, currencyB)] = rate
+
+    G = nx.DiGraph()
+    G.add_weighted_edges_from(data)     # O(n**3) -> Demora!
+
+    # Salvar os resultados obtidos
+    try:
+        with open('pickles/floyd_warshall_predecessor.pickle', 'rb') as pkl:
+            predecessors = pickle.load(pkl)
+    
+    except FileNotFoundError:
+        predecessors, _ = nx.floyd_warshall_predecessor_and_distance(G)
+        with open('pickles/floyd_warshall_predecessor.pickle', 'wb') as pkl:
+            pickle.dump(predecessors, pkl)
+
+    lst = nx.reconstruct_path('BRL', 'USD', predecessors)
+    print(lst)
+
+    money = 1000
+
+    for i in range(len(lst)-1):
+        tpl = (lst[i], lst[i+1])
+        money = convert[tpl] * money
+        print(money)
