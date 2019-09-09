6e9735f946f991a324cf47f8af62df8035c2a8d1
Druval Carvalho <durvalcsouza@outlook.com>
2019-09-08 22:25:29 -0300

    bellman ford implementation

--- api/fetch_data.py
+++ api/fetch_data.py
@@ -6,16 +6,55 @@ from exchange_locales import (
     CurrencyConverter
 )
 
+import pickle
+import matplotlib.pyplot as plt
+from collections import defaultdict
+
+from math import log2
+
+# The name of the currencies between the exchange_houses de change is 
+# different (BRL_VF, BRL_VM, BRL_DU). 
+# In order to exchange currencies between different exchange houses, it is 
+# necessary create to create a vertex representing the currency (BRL) and 
+# link all exchange offices.
+def add_junction_currency_between_exchange_houses(edges):
+    junction = set()
+
+    for currencyA, currencyB, rate in edges:
+        moneyA = currencyA[:3]
+        moneyB = currencyB[:3]
+
+        junction.add((moneyA, currencyA, 1))
+        junction.add((currencyA, moneyA, 1))
+
+        junction.add((moneyB, currencyB, 1))
+        junction.add((currencyB, moneyB, 1))
+    
+    return list(junction)
+
+def bellman_ford(ref, edges):
+    
+    dist = defaultdict(lambda:float('inf'))
+    pred = defaultdict(lambda:float(-1))
+
+    dist[ref] = 0
+    pred[ref] = ref
+
+    for i in range(len(edges)):
+        for u,v,w in edges:
+            if dist[v] > dist[u] + w:
+                dist[v] = dist[u] + w
+                pred[v] = u
+    
+    return dist, pred
+
+
 if __name__ == '__main__':
+    edges = FixerIO().edges + CurrencyLayer().edges + ExchangeRatesAPI().edges + CurrencyConverter().edges
     
-    FixerIO()
-    print('25%')
+    edges += add_junction_currency_between_exchange_houses(edges)
 
-    CurrencyLayer()
-    print('50%')
-    
-    ExchangeRatesAPI()
-    print('75%')
+    # jump cat -> max mult to min sum
+    edges = [ (u,v, -log2(w)) for u,v,w in edges ]
 
-    CurrencyConverter()
-    print('100%')
+    brl_to, pred = bellman_ford('BRL' ,edges)
