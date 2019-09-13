264b0d74475c225fbcdc7dac2cca96fc5cd57b70
Victor Moura <victor_cmoura@hotmail.com>
2019-09-13 08:29:12 -0300

    Adds CORS configs to flask api

--- api/api.py
+++ api/api.py
@@ -1,8 +1,13 @@
 from flask_api import FlaskAPI
+from flask_cors import CORS, cross_origin
 
 app = FlaskAPI(__name__)
 
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 @app.route('/')
+@cross_origin()
 def hello_world():
     return {'hello': 'world', 'salve': 1232123}
 
