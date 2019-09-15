619f3d62d53169d30fda6ba8760e75a95b21f737
Durval Carvalho <durvalcsouza@outlook.com>
2019-09-15 11:16:13 -0300

    Add static files to localhost

--- api/api.py
+++ api/api.py
@@ -1,15 +1,15 @@
 from flask_api import FlaskAPI
-from flask import request
+from flask import request, render_template
 from flask_cors import CORS, cross_origin
 from fetch_data import get_result
 from get_country_data import get_country_data
 
-app = FlaskAPI(__name__)
+app = FlaskAPI(__name__, static_url_path='')
 
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
-@app.route('/')
+@app.route('/converter/')
 @cross_origin()
 def currency_converter():
     
@@ -22,6 +22,11 @@ def currency_converter():
     result = get_result(**params)
 
     return result
+
+@app.route('/')
+@cross_origin()
+def root():
+    return render_template("index.html")
 
 @app.route('/country_data')
 @cross_origin()
